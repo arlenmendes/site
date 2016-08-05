7d504c6c5f8454322ebab4aa3168cee70842de93
arlenmendes <arlenmsilva@gmail.com>
2016-08-05 18:39:27 -0300

    Add files via upload

---
+++ index.php
@@ -0,0 +1,20 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+	<meta charset="UTF-8">
+	<title>Arlen Mendes</title>
+	<link rel="stylesheet" href="assets/css/materialize.min.css">
+	<link rel="stylesheet" href="assets/css/bootstrap.min.css">
+	<link rel="stylesheet" href="assets/css/style.css">
+</head>
+<body>
+	<section  id="header">
+		<div class="container">
+			<h1>
+				Arlen Mendes
+			</h1>
+			<h4>Desenvolvimento <span>Web</span> e Gestão de <span>Projetos</span></h4>
+		</div>
+	</section>
+</body>
+</html>
